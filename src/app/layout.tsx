b0a7a850467962d2467614ccde2d557b41b2642f
iam-roymwangi <royimwangi@gmail.com>
2026-06-05 20:04:04 +0300

    Added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import IslandNav from "@/components/IslandNav";
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className="min-h-screen bg-zinc-950 font-sans text-white pb-[calc(5.5rem+env(safe-area-inset-bottom))]">
         {children}
         <IslandNav />
+        <Analytics />
       </body>
     </html>
   );
